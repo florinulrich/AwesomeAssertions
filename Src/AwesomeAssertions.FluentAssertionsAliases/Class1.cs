2224fa624285d15f4ad3d4956d93db73b95c600b
florinulrich <45395978+florinulrich@users.noreply.github.com>
2025-02-09 17:01:54 +0100

    Add Aliases projects and approval tests

---
+++ Src/AwesomeAssertions.FluentAssertionsAliases/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AwesomeAssertions.FluentAssertionsAliases
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tests/FluentAssertionAliases.Approval.Tests/ApiApproval.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using PublicApiGenerator;
+using VerifyTests;
+using VerifyTests.DiffPlex;
+using VerifyXunit;
+using Xunit;
+
+namespace FluentAssertionAliases.Approval.Tests;
+
+public class ApiApproval
+{
+    static ApiApproval() => VerifyDiffPlex.Initialize(OutputType.Minimal);
+
+    [Theory]
+    [ClassData(typeof(TargetFrameworksTheoryData))]
+    public Task ApproveApi(string framework)
+    {
+        var configuration = typeof(ApiApproval).Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>()!.Configuration;
+        var assemblyFile = CombinedPaths("Src", "AwesomeAssertions", "bin", configuration, framework, "AwesomeAssertions.dll");
+        var assembly = Assembly.LoadFile(assemblyFile);
+        var publicApi = assembly.GeneratePublicApi(options: null);
+
+        return Verifier
+            .Verify(publicApi)
+            .ScrubLinesContaining("FrameworkDisplayName")
+            .UseDirectory(Path.Combine("ApprovedApi", "AwesomeAssertions"))
+            .UseFileName(framework)
+            .DisableDiff();
+    }
+
+    private class TargetFrameworksTheoryData : TheoryData<string>
+    {
+        public TargetFrameworksTheoryData()
+        {
+            var csproj = CombinedPaths("Src", "AwesomeAssertions", "AwesomeAssertions.csproj");
+            var project = XDocument.Load(csproj);
+            var targetFrameworks = project.XPathSelectElement("/Project/PropertyGroup/TargetFrameworks");
+            AddRange(targetFrameworks!.Value.Split(';'));
+        }
+    }
+
+    private static string GetSolutionDirectory([CallerFilePath] string path = "") =>
+        Path.Combine(Path.GetDirectoryName(path)!, "..", "..");
+
+    private static string CombinedPaths(params string[] paths) =>
+        Path.GetFullPath(Path.Combine(paths.Prepend(GetSolutionDirectory()).ToArray()));
+}
